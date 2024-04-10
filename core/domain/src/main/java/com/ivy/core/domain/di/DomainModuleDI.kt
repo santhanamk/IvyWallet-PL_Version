2294eabad51c9d285e0b5315603f7e3507ff0601
K S <lilberah@yahoo.com>
2024-04-09 23:12:09 -0700

    Complicated flow test done.

--- core/domain/src/main/java/com/ivy/core/domain/di/DomainModuleDI.kt
+++ core/domain/src/main/java/com/ivy/core/domain/di/DomainModuleDI.kt
@@ -1,10 +1,19 @@
 package com.ivy.core.domain.di
 
+import com.ivy.core.domain.pure.util.DispatcherProvider
+import com.ivy.core.domain.pure.util.StandardDispatchers
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.test.TestDispatcher
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DomainModuleDI {
+
+    @Provides
+    fun provideDispatcherProvider(): DispatcherProvider {
+        return StandardDispatchers()
+    }
 }
